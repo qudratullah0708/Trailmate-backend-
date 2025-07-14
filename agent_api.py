28b06b4406ddfc9aa1c765a0168ff4442996d37d
Qudrat Ullah <113133016+qudratullah0708@users.noreply.github.com>
2025-07-14 23:48:16 +0500

    Update agent_api.py url cors

--- agent_api.py
+++ agent_api.py
@@ -32,7 +32,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://my-production-site.com"
+    "https://trail-mate-ai-agent-7sgi.vercel.app"
 ]
 
 app.add_middleware(
