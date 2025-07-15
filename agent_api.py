3cbb65d9b56bd4a678bc7798262f29da9227a660
Qudrat Ullah <113133016+qudratullah0708@users.noreply.github.com>
2025-07-15 11:09:37 +0500

    Update agent_api.py

--- agent_api.py
+++ agent_api.py
@@ -32,6 +32,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://trail-mate-ai-agent-fok3.vercel.app",
     "https://trail-mate-ai-agent-7sgi.vercel.app"
 ]
 
